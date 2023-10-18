90b42fdb693285f95bdcd50ea94efac0916bc0cd
Stoyan <stoyan.kostadinov85@gmail.com>
2023-10-18 11:35:27 +0300

    enhance-registration-ui
    Implemented enhanced validation on registration page including focused error handling, useRef for input focus, and better error messages for specific Firebase errors.

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,17 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet-async";
-import {
-  getAuth,
-  browserLocalPersistence,
-  createUserWithEmailAndPassword,
-} from "firebase/auth";
+import { getAuth, browserLocalPersistence, createUserWithEmailAndPassword } from "firebase/auth";
+
+interface FirebaseError {
+  code?: string;
+  message: string;
+}
 
 const RegisterPage: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
   const [formError, setFormError] = useState("");
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const repeatPasswordRef = useRef<HTMLInputElement>(null);
 
   const MIN_PASS_LENGTH = 8;
 
@@ -26,11 +31,13 @@ const RegisterPage: React.FC = () => {
 
     if (!isValidEmail(email)) {
       setFormError("Please enter a valid email address.");
+      emailRef.current?.focus();
       return;
     }
 
     if (password !== repeatPassword) {
       setFormError("Passwords do not match.");
+      repeatPasswordRef.current?.focus();
       return;
     }
 
@@ -38,6 +45,7 @@ const RegisterPage: React.FC = () => {
       setFormError(
         `Password should be at least ${MIN_PASS_LENGTH} characters long.`
       );
+      passwordRef.current?.focus();
       return;
     }
 
@@ -46,8 +54,23 @@ const RegisterPage: React.FC = () => {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
     } catch (error) {
-      if (error instanceof Error) {
-        setFormError(error.message);
+      const firebaseError = error as FirebaseError;
+
+      if (firebaseError.code) {
+        switch (firebaseError.code) {
+          case "auth/email-already-in-use":
+            setFormError(
+              "The email address is already in use by another account."
+            );
+            emailRef.current?.focus();
+            break;
+          case "auth/weak-password":
+            setFormError("The password is too weak.");
+            passwordRef.current?.focus();
+            break;
+          default:
+            setFormError(firebaseError.message);
+        }
       } else {
         setFormError("An unexpected error occurred.");
       }
@@ -55,7 +78,7 @@ const RegisterPage: React.FC = () => {
   };
 
   return (
-    <div className="container login-register animate__animated animate__flipInX">
+    <div className="container login-register animate__animated animate__fadeIn">
       <Helmet>
         <title>Create Account</title>
         <meta
@@ -72,6 +95,8 @@ const RegisterPage: React.FC = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           className="input"
+          aria-label="Email"
+          ref={emailRef}
         />
         <input
           id="password"
@@ -80,6 +105,8 @@ const RegisterPage: React.FC = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           className="input"
+          aria-label="Password"
+          ref={passwordRef}
         />
         <input
           id="repeat-password"
@@ -88,6 +115,8 @@ const RegisterPage: React.FC = () => {
           value={repeatPassword}
           onChange={(e) => setRepeatPassword(e.target.value)}
           className="input"
+          aria-label="Repeat Password"
+          ref={repeatPasswordRef}
         />
         <button type="submit" className="btn yellow stretch extra-padding">
           Register
@@ -99,7 +128,9 @@ const RegisterPage: React.FC = () => {
         <Link to="/login">Login</Link>
       </p>
       {formError && (
-        <p className="error animate__animated animate__bounceIn">{formError}</p>
+        <p className="error animate__animated animate__bounceIn" role="alert">
+          {formError}
+        </p>
       )}
     </div>
   );
