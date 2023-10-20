a0604d5bbaa3c1457f497ef929446a73ec996a3b
Stoyan <stoyan.kostadinov85@gmail.com>
2023-10-20 11:35:18 +0300

    feature/user-profile-enhancements
    Added creationTime and lastSignInTime metadata to the UserProfile component and updated the component's styling for better user clarity.

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { formatDate } from "../utils/formatDate";
 
 interface User {
   email: string;
   uid: string;
-  joinedDate: Date;
+  metadata?: {
+    creationTime?: string;
+    lastSignInTime?: string;
+  };
 }
 
 interface ReduxState {
@@ -15,14 +19,19 @@ const UserProfile: React.FC<ReduxState> = ({ user, roles }) => {
 
   if (!user) return null;
 
+  const creationDate = user.metadata?.creationTime;
+  const lastLoginDate = user.metadata?.lastSignInTime;
+
+  if (!creationDate) {
+    console.error('No joined date or creation time found for user:', user);
+    return null;
+  }
+
+  const formattedDate = formatDate(new Date(creationDate));
+  const formattedLastLoginDate = lastLoginDate ? formatDate(new Date(lastLoginDate)) : "Unknown";
+
   const name = user.email.split("@")[0];
   const role = roles[user.uid] || "Member";
-
-  const formattedDate = new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  }).format(user.joinedDate);
 
   return (
     <div className="user-profile animate__animated animate__fadeIn">
@@ -30,19 +39,20 @@ const UserProfile: React.FC<ReduxState> = ({ user, roles }) => {
         <span className="material-symbols-outlined">emoji_people</span>
         Hello, {name}
       </h2>
-
-      <p>It's good to see you!</p>
-
-      <h3>User Details</h3>
-      <p>
-        <strong>Email:</strong> {user.email}
-      </p>
-      <p>
-        <strong>Role:</strong> {role}
-      </p>
-      <p>
-        <strong>Joined Date:</strong> {formattedDate}
-      </p>
+      <p className="user-profile__greetings">It's good to see you!</p>
+      <h4>User Details:</h4>
+      <p>{user.email}</p>
+      <div className="user-profile__details">
+        <p>
+          <strong>Your Role:</strong> <span className="role">{role}</span>
+        </p>
+        <p>
+          <strong>Joined Date:</strong> <span>{formattedDate}</span>
+        </p>
+        <p>
+          <strong>Last Login:</strong> <span>{formattedLastLoginDate}</span>
+        </p>
+      </div>
     </div>
   );
 };
