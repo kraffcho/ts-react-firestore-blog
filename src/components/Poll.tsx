15f46115a6243d3d8e7883c896ef2bf06e1cb0a1
Stoyan <stoyan.kostadinov85@gmail.com>
2023-10-24 17:58:47 +0300

    refactor-poll-page
    Refactors and improves the Poll page components, ensuring type safety and enhanced code readability.

--- src/components/Poll.tsx
+++ src/components/Poll.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { fetchIP, fetchPollData, updatePollVote } from "../utils/fetchPoll";
 import { PollData } from "../utils/types";
 
@@ -13,24 +13,11 @@ const Poll: React.FC<PollProps> = ({ pollId }) => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  const { options = {}, votedIPs = [] } = poll || {};
-  const hasVoted = userIP && votedIPs.includes(userIP);
-  const totalVotes = Object.values(options).reduce((acc, option) => acc + option.votes, 0);
-  const getVoteString = (votes: number) => `${votes} ${votes === 1 ? "vote" : "votes"}`;
-  const handleVote = async () => {
-    if (!poll || !selectedOption || !userIP || votedIPs.includes(userIP)) {
-      console.log("You have already voted or data is missing.");
-      return;
-    }
-
-    try {
-      const updatedPoll = await updatePollVote(poll, selectedOption, userIP, pollId);
-      setPoll(updatedPoll);
-    } catch (error) {
-      console.error("Error updating vote:", error);
-      setError('Failed to update your vote. Please try again.');
-    }
-  };
+  const hasVoted = Boolean(userIP && poll?.votedIPs.includes(userIP));
+  const totalVotes = Object.values(poll?.options || {}).reduce(
+    (acc, option) => acc + option.votes,
+    0
+  );
 
   useEffect(() => {
     const fetchData = async () => {
@@ -38,80 +25,139 @@ const Poll: React.FC<PollProps> = ({ pollId }) => {
       try {
         const ip = await fetchIP();
         const pollData = await fetchPollData(pollId);
-        
+
         setUserIP(ip);
         setPoll(pollData);
       } catch (err) {
-        setError('Failed to fetch data. Please try again later.');
+        setError("Failed to fetch data. Please try again later.");
       } finally {
         setLoading(false);
       }
     };
-    
+
     fetchData();
   }, [pollId]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const handleVote = async () => {
+    if (!poll || !selectedOption || hasVoted) {
+      console.log("You have already voted or data is missing.");
+      return;
+    }
 
-  if (error) {
-    return <div>{error}</div>;
-  }
+    try {
+      const updatedPoll = await updatePollVote(
+        poll,
+        selectedOption,
+        userIP!,
+        pollId
+      );
+      setPoll(updatedPoll);
+    } catch (error) {
+      console.error("Error updating vote:", error);
+      setError("Failed to update your vote. Please try again.");
+    }
+  };
 
-  if (!poll) {
-    return <div>No poll data available.</div>;
-  }
+  if (loading) return <LoadingState />;
+  if (error) return <ErrorState error={error} />;
+  if (!poll) return <NoPollState />;
 
   return (
-    <div className="poll-container animate__animated animate__fadeIn">
-      <h2 className="poll-title">
-        <span className="material-symbols-outlined">ballot</span>
-        Poll Question
-      </h2>
-      <p className="poll-question">{poll.question}</p>
-      <div className="poll-options">
-        {Object.entries(poll.options).map(([key, option]) => {
-          const percentage = totalVotes
-            ? ((option.votes / totalVotes) * 100).toFixed(1)
-            : 0;
-          const voteString = getVoteString(option.votes);
+    <PollLayout
+      poll={poll}
+      totalVotes={totalVotes}
+      hasVoted={hasVoted}
+      handleVote={handleVote}
+      setSelectedOption={setSelectedOption}
+    />
+  );
+};
 
-          return (
-            <div key={key} className="poll-option">
-              <div
-                className="percentage-background"
-                style={{ width: `${percentage}%` }}
-              ></div>
-              {hasVoted ? (
-                <span className="option-text">
-                  {option.text} ({voteString}) - {percentage}%
-                </span>
-              ) : (
-                <>
-                  <input
-                    type="radio"
-                    id={key}
-                    className="option-input"
-                    name="poll-option"
-                    value={key}
-                    onChange={() => setSelectedOption(key)}
-                  />
-                  <label htmlFor={key} className="option-label">
-                    {option.text} ({voteString})
-                  </label>
-                </>
-              )}
-            </div>
-          );
-        })}
-      </div>
+const LoadingState = () => <div>Loading...</div>;
+
+const ErrorState: React.FC<{ error: string }> = ({ error }) => (
+  <div>{error}</div>
+);
+
+const NoPollState = () => <div>No poll data available.</div>;
+
+const PollLayout: React.FC<{
+  poll: PollData;
+  totalVotes: number;
+  hasVoted: boolean;
+  handleVote: () => void;
+  setSelectedOption: (key: string) => void;
+}> = ({ poll, totalVotes, hasVoted, handleVote, setSelectedOption }) => (
+  <div className="poll-container animate__animated animate__fadeIn">
+    <h2 className="poll-title">
+      <span className="material-symbols-outlined">ballot</span>
+      Poll Question
+    </h2>
+    <p className="poll-question">{poll.question}</p>
+    <div className="poll-options">
+      {Object.entries(poll.options).map(([key, option]) => (
+        <PollOption
+          key={key}
+          id={key}
+          option={option}
+          totalVotes={totalVotes}
+          hasVoted={hasVoted}
+          setSelectedOption={setSelectedOption}
+        />
+      ))}
+    </div>
+    {hasVoted ? (
+      <p className="voted-message">Your vote has been counted. Thanks!</p>
+    ) : (
+      <button
+        className="vote-button btn green extra-padding"
+        onClick={handleVote}
+      >
+        Cast Your Vote
+      </button>
+    )}
+  </div>
+);
+
+const PollOption: React.FC<{
+  id: string;
+  option: {
+    text: string;
+    votes: number;
+  };
+  totalVotes: number;
+  hasVoted: boolean;
+  setSelectedOption: (key: string) => void;
+}> = ({ id, option, totalVotes, hasVoted, setSelectedOption }) => {
+  const percentage = totalVotes
+    ? ((option.votes / totalVotes) * 100).toFixed(1)
+    : "0";
+  const voteString = `${option.votes} ${option.votes === 1 ? "vote" : "votes"}`;
+
+  return (
+    <div className="poll-option">
+      <div
+        className="percentage-background"
+        style={{ width: `${percentage}%` }}
+      ></div>
       {hasVoted ? (
-        <p className="voted-message">Your vote has been counted. Thanks!</p>
+        <span className="option-text">
+          {option.text} ({voteString}) - {percentage}%
+        </span>
       ) : (
-        <button className="vote-button btn green extra-padding" onClick={handleVote}>
-          Cast Your Vote
-        </button>
+        <>
+          <input
+            type="radio"
+            id={id}
+            className="option-input"
+            name="poll-option"
+            value={id}
+            onChange={() => setSelectedOption(id)}
+          />
+          <label htmlFor={id} className="option-label">
+            {option.text} ({voteString})
+          </label>
+        </>
       )}
     </div>
   );
