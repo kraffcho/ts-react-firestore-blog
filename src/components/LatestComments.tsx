6585318e9bc9db1ee5d5041c62e3dcd7caaca0be
Stoyan <stoyan.kostadinov85@gmail.com>
2023-10-24 17:41:02 +0300

    feature/expandable-latest-comments
    Introduce functionality to display the initial 6 latest comments and allow users to expand the view by clicking a button, revealing up to 4 additional comments at a time.

--- src/components/LatestComments.tsx
+++ src/components/LatestComments.tsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import CommentItem from "./CommentItem";
 import { useLatestComments } from "../hooks/useLatestComments";
 
 const LatestComments: React.FC = () => {
   const { latestComments, posts } = useLatestComments();
+  const [visibleCommentsCount, setVisibleCommentsCount] = useState(6);
 
   return (
     <div className="latest-comments animate__animated animate__fadeIn">
       <h2>
-        <span className="material-symbols-outlined">comment</span> Latest Comments
+        <span className="material-symbols-outlined">comment</span> Latest
+        Comments
       </h2>
-      {latestComments.map((comment) => (
+      {latestComments.slice(0, visibleCommentsCount).map((comment) => (
         <CommentItem
           key={comment.id}
           comment={comment}
           postTitle={posts[comment.postId]?.title}
         />
       ))}
+      {latestComments.length > visibleCommentsCount && (
+        <button
+          className="btn green extra-padding show-more-comments"
+          onClick={() => setVisibleCommentsCount((prev) => prev + 4)}
+        >
+          Show more comments
+        </button>
+      )}
     </div>
   );
 };
