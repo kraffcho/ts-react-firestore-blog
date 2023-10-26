3eb2b78d30c4e4dd3ac9690d781f0042c8eb50d6
Stoyan Kostadinov <stoyan.kostadinov85@gmail.com>
2023-10-27 00:01:17 +0300

    feature/404-random-video-bg
    Update the 404 "Not Found" page to display a random video as the background for enhanced user experience.

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 
 const NotFoundPage: React.FC = () => {
+  const videoNum = Math.floor(Math.random() * 4) + 1;
   return (
     <section className="container animate__animated animate__fadeIn">
       <Helmet>
@@ -11,17 +12,8 @@ const NotFoundPage: React.FC = () => {
           content="The page you are looking for does not exist. Please check the URL and try again."
         />
       </Helmet>
+      <video src={`../assets/videos/not-found-${videoNum}.mp4`} autoPlay loop muted playsInline></video>
       <div className="not-found-wrapper">
-        <svg
-          className="magnifier-svg animate__animated animate__jello"
-          width="200"
-          height="200"
-          viewBox="0 0 24 24"
-        >
-          <circle cx="11" cy="11" r="7" stroke="#333" strokeWidth="2" fill="none" />
-          <line x1="16" y1="16" x2="21" y2="21" stroke="#333" strokeWidth="2" />
-        </svg>
-
         <h2 className="heading animate__animated animate__fadeInDown">Page Not Found</h2>
         <p className="animate__animated animate__fadeInUp">
           The page you are looking for does not exist.<br />Please check the URL and try again.
